43b8d61ce7ad719193d43631e8a57a8cd6af0338
Jondesan <jonde.huhtasalo@gmail.com>
2024-11-05 13:15:40 +0200

    Add file writing for output

--- run/run_hf.py
+++ run/run_hf.py
@@ -16,26 +16,26 @@ if __name__ == '__main__':
     mols = args.mols
 
     basis_sets = ['aug-pc-1', 'aug-pc-2', 'aug-pc-3', 'aug-pc-4']
+    with open('hf_energies.out') as outfile:
+        for molpath in mols:
+            for bs in basis_sets:
+                for unc in ['', 'unc-']:
+                    fnparts = molpath.split('/')[-1].split('.')
+                    if len(fnparts) > 2:
+                        charge = [int(substring.replace('charge', '')) for substring in fnparts if 'charge' in substring]
+                        charge = charge[0] if len(charge) != 0 else 0
+                        spin = [int(substring.replace('spin','')) for substring in fnparts if 'spin' in substring]
+                        spin = spin[0] if len(spin) != 0 else None
+                    else:
+                        charge = 0
+                        spin = None
+                    mol = pyscf.M(
+                        atom=molpath, basis=unc + bs,
+                        charge=charge, spin=spin,
+                        verbose=0)
 
-    for molpath in mols:
-        for bs in basis_sets:
-            for unc in ['', 'unc-']:
-                fnparts = molpath.split('/')[-1].split('.')
-                if len(fnparts) > 2:
-                    charge = [int(substring.replace('charge', '')) for substring in fnparts if 'charge' in substring]
-                    charge = charge[0] if len(charge) != 0 else 0
-                    spin = [int(substring.replace('spin','')) for substring in fnparts if 'spin' in substring]
-                    spin = spin[0] if len(spin) != 0 else None
-                else:
-                    charge = 0
-                    spin = None
-                mol = pyscf.M(
-                    atom=molpath, basis=unc + bs,
-                    charge=charge, spin=spin,
-                    verbose=0)
+                    mf = pyscf.scf.HF(mol)
+                    mf.init_guess = 'atom'
+                    mf.kernel()
 
-                mf = pyscf.scf.HF(mol)
-                mf.init_guess = 'atom'
-                mf.kernel()
-
-                print(molpath.split('/')[-1], unc+bs, mf.e_tot)
+                    outfile.write(f'{molpath.split("/")[-1]}\t{unc+bs}\t{mf.e_tot}\n')
