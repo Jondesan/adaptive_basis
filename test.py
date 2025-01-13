91f0f74bbb201463afe465b9fa2bf227cdf67320
Jondesan <jonde.huhtasalo@gmail.com>
2025-01-13 21:25:28 +0200

    add test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import adb
+import pyscf
+import numpy as np
+
+if __name__ == '__main__':
+    mol = pyscf.M(
+        atom='/home/joonahuh/uni/electronic_structure/benchmarks/pom_geom/weigend_ahlrichs/output/be2f4.xyz',
+        basis='def2-tzvp',
+        # basis='321G',
+        verbose=0)
+
+    # mf = pyscf.scf.HF(mol)
+    # F = mf.get_fock()
+    # S = mf.get_ovlp()
+    adb.atomic_block_minimal_basis(mol)
