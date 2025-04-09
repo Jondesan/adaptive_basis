043aa22e5e580430db92886f30d56e477fe46dc9
joonahuh <joonahuh@2001-14ba-a0b5-5820--eae.rev.dnainternet.fi>
2025-04-09 12:32:51 +0300

    Local changes

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 if __name__ == '__main__':
     mol = pyscf.M(
-        atom='/home/joonahuh/uni/electronic_structure/benchmarks/pom_geom/weigend_ahlrichs/output/be2f4.xyz',
+        atom='be2f4.xyz',
         basis='def2-tzvp',
         # basis='321G',
         verbose=0)
