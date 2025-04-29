20904a91e49414042639e3746ab3f48068ab285c
Jondesan <jonde.huhtasalo@gmail.com>
2025-04-29 17:00:27 +0300

    Add line continuation

--- adbmodule/adbutils.py
+++ adbmodule/adbutils.py
@@ -514,7 +514,7 @@ def run_psi4(
         # determine which has lowest converged energy
         unique_occs = list(set(occs))
 
-        print(f'Found the following occupations:\n{"\n\n".join(
+        print(f'Found the following occupations:\n{"\n\n".join( \
             map(lambda x: "\n".join(x), unique_occs))}')
         print('Testing which provides lowest converged energy...')
         doccs = []
