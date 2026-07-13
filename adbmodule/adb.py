73853afea34c79a1b416cb3d51e0c90b4cca54c2
Joonatan Huhtasalo <jonde.huhtasalo@gmail.com>
2026-07-13 11:32:48 +0300

    move mask functions to mask.py

--- adbmodule/adb.py
+++ adbmodule/adb.py
@@ -5,7 +5,7 @@
 # import pyscf
 import numpy as np
 from scipy import linalg
-from itertools import count
+# from itertools import count
 from pyscf.gto.basis.parse_nwchem import convert_basis_to_nwchem,\
     to_general_contraction, convert_ecp_to_nwchem
 from pyscf.gto.ecp import core_configuration
@@ -24,6 +24,7 @@ import copy
 import sys
 import re
 from calculations import eig, symmetrized_eig, get_iteration_criteria_value, get_q_sqrd
+from mask import init_smask, mask_to_smask, smask_to_mask, set_linked_shells, linked_shell_idx, get_atom_shell_label
 from CONSTANTS import NFUNCS
 
 
@@ -263,187 +264,10 @@ def get_shells(mol: gto.MoleBase) -> np.ndarray:
 
     if sum(shells) != mol.nao_nr():
         raise Exception(
-            "Number of functions in the mask does not correspond with number of functions of the molecule!"
+            "Number of functions in the mask does not correspond with number of functions in the molecule!"
         )
 
     return shells
-
-
-def maskidx_to_smaskidx(
-        mask:   np.ndarray,
-        smask:  np.ndarray,
-        cart:   bool        = False) -> list:
-    """Create mapping between mask and smask"""
-    mapping = [0] * len(mask)
-    counter = 0
-    for i, sm in enumerate(smask):
-        angl = sm[2]
-        for j in range((angl + 1) * (angl + 2) // 2 if cart else 2 * angl + 1):
-            mapping[counter] = i
-            counter += 1
-    return mapping
-
-
-def init_smask(
-        mol:    gto.MoleBase,
-        cart:   bool          = False) -> np.ndarray:
-    """Initialize the shell mask array. smask will be a list of lists,
-    with length equal to the number of uncontracted shells, and each
-    element is a two element list, first is bool that specifies the mask
-    for the current shell, the other how many primitives in this shell.
-    """
-
-    smask = []
-
-    count = np.zeros((mol.natm, 9), dtype=int)
-    for ib in range(mol.nbas):
-        ia = mol.bas_atom(ib)       # atom that given basis function sits on
-        angl = mol.bas_angular(ib)  # angular momentum angl of given basis function
-        nc = mol.bas_nctr(ib)       # number of CGTOs for given shell
-        symb = mol.atom_symbol(ia)  # label of given atom
-        nelec_ecp = mol.atom_nelec_core(ia)  # Number of ecp electrons
-        if nelec_ecp == 0 or angl > 3:
-            shl_start = count[ia, angl] + angl + 1
-        else:
-            coreshl = core_configuration(nelec_ecp, atom_symbol = _std_symbol(symb))
-            shl_start = coreshl[angl] + count[ia, angl] + angl + 1
-        count[ia, angl] += nc
-        for n in range(shl_start, shl_start + nc):
-            if nelec_ecp == 0 or angl > 3:
-                n_remove_ecp = n
-            else:
-                n_remove_ecp = n - coreshl[angl]
-            smask.append(
-                [
-                    False,
-                    (angl + 1) * (angl + 2) // 2 if cart else 2 * angl + 1,
-                    angl,
-                    (ia, symb, n, lib.param.ANGULAR[angl].capitalize(), n_remove_ecp),
-                ]
-            )
-
-    return np.array(smask, dtype=object)
-
-
-def smask_to_mask(
-        smask:  np.ndarray,
-        cart:   bool        = False) -> np.ndarray:
-    """Convert current shell mask into function mask."""
-    funcs_per_shell = [
-        ((s[2] + 1) * (s[2] + 2) // 2 if cart else 2 * s[2] + 1) for s in smask
-    ]
-    mask = [False] * sum(funcs_per_shell)
-    for i, sm in enumerate(smask):
-        if sm[0]:
-            rb = sum(funcs_per_shell[:i])
-            re = rb + sm[1]
-            mask[rb:re] = [True] * (re - rb)
-    return np.array(mask, dtype=bool)
-
-
-def mask_to_smask(
-        mask:   np.ndarray,
-        smask:  np.ndarray,
-        cart:   bool        = False) -> np.ndarray:
-    """Flip shells of smask to True that have 1 or more functions set to
-    True in mask.
-    """
-    mapping = maskidx_to_smaskidx(mask, smask, cart)
-    for i in np.argwhere(mask):
-        smask[mapping[i[0]]][0] = True
-
-    return smask
-
-
-def linked_shell_idx(smask: np.ndarray) -> np.ndarray:
-    """ Return smask indices that correspond to duplicate shells, i.e.
-    if molecule has more than one of same atom type, the shells of that
-    atom will be duplicated.
-
-    Args:
-        smask : ndarray
-            Shell mask array
-
-    Returns:
-        shl_indices : ndarray
-            Duplicate shell indices
-    """
-    atoms_found = []
-    shells = ["".join([str(s) for s in sm[3][1:]]) for sm in smask]
-
-    shl_indices = []
-    for i, sm in enumerate(smask):
-        if "".join([str(s) for s in sm[3][1:]]) not in atoms_found:
-            atoms_found.append("".join([str(s) for s in sm[3][1:]]))
-            indices = [
-                ind
-                for ind, ele in zip(count(), shells)
-                if ele == "".join([str(s) for s in sm[3][1:]])
-            ]
-            shl_indices.append(indices)
-    return shl_indices
-
-
-def get_all_shell_labels(mol: gto.MoleBase) -> list[str]:
-    count = np.zeros((mol.natm, 9), dtype=int)
-    labels = []
-    for ib in range(mol.nbas):  # nbas = number of shells (basis fcts)
-        ia = mol.bas_atom(ib)   # atom that given basis function sits on
-        l = mol.bas_angular(ib) # angular momentum l of basis function
-        strl = lib.param.ANGULAR[l] # angular momentum label
-        nc = mol.bas_nctr(ib)   # number of CGTOs for given shell
-        symb = mol.atom_symbol(ia)  # label of given atom
-        nelec_ecp = mol.atom_nelec_core(ia) # Number of ecp electrons
-
-        if nelec_ecp == 0 or l > 3:
-            shl_start = count[ia,l]+l+1
-        else:
-            coreshl = core_configuration(nelec_ecp, atom_symbol=_std_symbol(symb))
-            shl_start = coreshl[l]+count[ia,l]+l+1
-        count[ia,l] += nc
-        for n in range(shl_start, shl_start+nc):
-            labels.append((ia, symb, '%d%s' % (n, strl)))
-
-    return labels
-
-
-def link_shells(mol, mask):
-    """Toggle functions corresponding to same atoms on and toggle all
-    functions within a shell.
-
-    Args:
-        mol : pyscf.got.MoleBase
-            The molecule object
-        mask : np.ndarray
-            The function mask
-    """
-
-    smask = mask_to_smask(
-        mask,
-        init_smask(mol, mol.cart),
-        mol.cart)
-
-    return smask_to_mask(smask, mol.cart)
-
-
-def get_atom_shell_label(
-        mol: gto.MoleBase,
-        shl_idx: int,
-        link_shells: bool = False
-    ) -> str:
-    labels = get_all_shell_labels(mol)
-
-    if link_shells:
-        return '%s %s' % labels[shl_idx][1:]
-    return '%d %s %s' % labels[shl_idx]
-
-
-def print_shells(mol: gto.MoleBase, smask: np.ndarray) -> None:
-    labels = get_all_shell_labels(mol)
-    for i,sm in enumerate(smask):
-        if not sm[0]:
-            continue
-        print('Atom %d, symb: %s, shell: %s' % labels[i])
 
 
 def get_sub_scf_attributes(
@@ -566,23 +390,6 @@ def print_data(
     print(f'  {diff:{">15s" if isinstance(diff, str) else "15.9f"}}', end="")
     print(f'  {E_scf:{">15s" if isinstance(E_scf, str) else "15.9f"}}', end="")
     print(f'  {Qsqrd:{">15s" if isinstance(Qsqrd, str) else "18.12f"}}')
-
-
-def set_linked_shells(
-        smask:  np.ndarray,
-        val:    bool        ) -> np.ndarray:
-    """Set smask to 'val' at linked shell positions.
-    """
-    copysmask = copy.deepcopy(smask)
-    selected_shells = np.argwhere([sm[0] for sm in copysmask])
-    all_shells = np.array([''.join(map(str, sm[3][1:])) for sm in copysmask])
-    same_as_selected = np.argwhere(
-        all_shells == all_shells[selected_shells]
-    )[:,1]
-    temp = copysmask[same_as_selected][:,0]
-    temp = val
-    copysmask[same_as_selected,0] = temp
-    return copysmask
 
 
 def mask_matrix(
